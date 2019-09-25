40fba7bc00051322acf8998b2124d875adb1e6b8
SYC0225 <52234637+SYC0225@users.noreply.github.com>
2019-09-25 09:12:56 +0800

    完善商品分类、详情

--- pages/goods/goods.vue
+++ pages/goods/goods.vue
@@ -210,16 +210,8 @@
 	export default {
 		data() {
 			return {
-<<<<<<< HEAD
 				details: {},
 				pid: "",
-=======
-<<<<<<< HEAD
-				details: {},
-				pid: "",
-=======
->>>>>>> 618a4249c0fc1d86d1270691ac503a6289cfb4e7
->>>>>>> 52474682771651986e60455ba31871df6d466b06
 				//控制渐变标题栏的参数
 				beforeHeaderzIndex: 11, //层级
 				afterHeaderzIndex: 10, //层级
@@ -230,31 +222,7 @@
 				showBack: true,
 				// #endif
 				//轮播主图数据
-<<<<<<< HEAD
 				swiperList: [],
-=======
-<<<<<<< HEAD
-				swiperList: [],
-=======
-				swiperList: [{
-						id: 1,
-						img: 'https://ae01.alicdn.com/kf/HTB1Mj7iTmzqK1RjSZFjq6zlCFXaP.jpg'
-					},
-					{
-						id: 2,
-						img: 'https://ae01.alicdn.com/kf/HTB1fbseTmzqK1RjSZFLq6An2XXaL.jpg'
-					},
-					{
-						id: 3,
-						img: 'https://ae01.alicdn.com/kf/HTB1dPUMThnaK1RjSZFtq6zC2VXa0.jpg'
-					},
-					{
-						id: 4,
-						img: 'https://ae01.alicdn.com/kf/HTB1OHZrTXzqK1RjSZFvq6AB7VXaw.jpg'
-					}
-				],
->>>>>>> 618a4249c0fc1d86d1270691ac503a6289cfb4e7
->>>>>>> 52474682771651986e60455ba31871df6d466b06
 				//轮播图下标
 				currentSwiper: 0,
 				anchorlist: [], //导航条锚点
@@ -268,46 +236,13 @@
 					name: "商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题",
 					price: "127.00",
 					number: 1,
-<<<<<<< HEAD
 					service: [],
 					spec: ["当面交易", "蓝沢担保", "线上发货"],
 					comment: {}
-=======
-<<<<<<< HEAD
-					service: [],
-					spec: ["当面交易", "蓝沢担保", "线上发货"],
-					comment: {}
-=======
-					service: [{
-							name: "正品保证",
-							description: "此商品官方保证为正品"
-						},
-						{
-							name: "极速退款",
-							description: "此商品享受退货极速退款服务"
-						},
-						{
-							name: "7天退换",
-							description: "此商品享受7天无理由退换服务"
-						}
-					],
-					spec: ["XS", "S", "M", "L", "XL", "XXL"],
-					comment: {
-						number: 102,
-						userface: '../../static/img/face.jpg',
-						username: '大黑哥',
-						content: '很不错，之前买了很多次了，很好看，能放很久，和图片色差不大，值得购买！'
-					}
->>>>>>> 618a4249c0fc1d86d1270691ac503a6289cfb4e7
->>>>>>> 52474682771651986e60455ba31871df6d466b06
 				},
 				selectSpec: null, //选中规格
 				isKeep: false, //收藏
 				//商品描述html
-<<<<<<< HEAD
-				descriptionStr: ""
-=======
-<<<<<<< HEAD
 				descriptionStr: ""
 			};
 		},
@@ -347,48 +282,6 @@
 		mounted() {
 
 		},
-=======
-				descriptionStr: '<div style="text-align:center;"><img width="100%" src="https://ae01.alicdn.com/kf/HTB1t0fUl_Zmx1VjSZFGq6yx2XXa5.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB1LzkjThTpK1RjSZFKq6y2wXXaT.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB18dkiTbvpK1RjSZPiq6zmwXXa8.jpg"/></div>'
->>>>>>> 52474682771651986e60455ba31871df6d466b06
-			};
-		},
-		onLoad(options) {
-			this.pid = options.pid;
-			console.log(this.pid, "(((((((((((((((((((())))))))))))))))))))")
-			// #ifdef MP
-
-			//小程序隐藏返回按钮
-			this.showBack = false;
-			// #endif
-			//option为object类型，会序列化上个页面传递的参数
-			console.log(options.pid); //打印出上个页面传递的参数。
-		},
-		onReady() {
-			this.calcAnchor(); //计算锚点高度，页面数据是ajax加载时，请把此行放在数据渲染完成事件中执行以保证高度计算正确
-			this.todetails() //请求商品的详细信息；
-		},
-		onPageScroll(e) {
-			//锚点切换
-			this.selectAnchor = e.scrollTop >= this.anchorlist[2].top ? 2 : e.scrollTop >= this.anchorlist[1].top ? 1 : 0;
-			//导航栏渐变
-			let tmpY = 375;
-			e.scrollTop = e.scrollTop > tmpY ? 375 : e.scrollTop;
-			this.afterHeaderOpacity = e.scrollTop * (1 / tmpY);
-			this.beforeHeaderOpacity = 1 - this.afterHeaderOpacity;
-			//切换层级
-			this.beforeHeaderzIndex = e.scrollTop > 0 ? 10 : 11;
-			this.afterHeaderzIndex = e.scrollTop > 0 ? 11 : 10;
-		},
-		//上拉加载，需要自己在page.json文件中配置"onReachBottomDistance"
-		onReachBottom() {
-			uni.showToast({
-				title: '触发上拉加载'
-			});
-		},
-		mounted() {
-
-		},
->>>>>>> 618a4249c0fc1d86d1270691ac503a6289cfb4e7
 		methods: {
 			//轮播图指示器
 			swiperChange(event) {
@@ -406,10 +299,6 @@
 					url: "../msg/chat/chat?name=客服008"
 				})
 			},
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 52474682771651986e60455ba31871df6d466b06
 			todetails() {
 				console.log("请求商品的详细数据");
 				uni.request({
@@ -532,11 +421,6 @@
 					}
 				})
 			},
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 618a4249c0fc1d86d1270691ac503a6289cfb4e7
->>>>>>> 52474682771651986e60455ba31871df6d466b06
 			// 分享
 			share() {
 				this.shareClass = 'show';
@@ -614,7 +498,6 @@
 				if (this.goodsData.number <= 1) {
 					return;
 				}
-<<<<<<< HEAD
 				// this.goodsData.number--;
 				this.goodsData.number = 1;
 			},
@@ -622,23 +505,6 @@
 			add() {
 				// this.goodsData.number++;
 				this.goodsData.number = 1;
-=======
-<<<<<<< HEAD
-				// this.goodsData.number--;
-				this.goodsData.number = 1;
-			},
-			//增加数量
-			add() {
-				// this.goodsData.number++;
-				this.goodsData.number = 1;
-=======
-				this.goodsData.number--;
-			},
-			//增加数量
-			add() {
-				this.goodsData.number++;
->>>>>>> 618a4249c0fc1d86d1270691ac503a6289cfb4e7
->>>>>>> 52474682771651986e60455ba31871df6d466b06
 			},
 			//跳转锚点
 			toAnchor(index) {
